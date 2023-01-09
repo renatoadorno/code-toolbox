889b48591861de91d1e68347b7c54af53c71d560
Renato Adorno <renatowillian.2000@gmail.com>
2023-01-09 02:08:08 -0300

    ✨ feat: adds template js-stitches

---
+++ react/js-stitches/stitches.config.js
@@ -0,0 +1,61 @@
+import { createStitches } from '@stitches/react';
+import { purple } from '@radix-ui/colors';
+
+export const {
+  styled,
+  createTheme,
+} = createStitches({
+  utils: {
+    mx: (value) => ({
+      marginRight: value,
+      marginLeft: value,
+    }),
+    my: (value) => ({
+      marginTop: value,
+      marginBottom: value,
+    }),
+
+    px: (value) => ({
+      paddingRight: value,
+      paddingLeft: value,
+    }),
+    py: (value) => ({
+      paddingTop: value,
+      paddingBottom: value,
+    }),
+
+    size: (value) => ({
+      width: value,
+      height: value,
+    }),
+  },
+
+  media: {
+    bp1: '(min-width: 350px) and (max-width: 450px)',
+    bp2: '(min-width: 480px)',
+    bp3: '(min-width: 500px)',
+    bp4: '(min-width: 500px)',
+    bp5: '(min-width: 500px)',
+    bp6: '(min-width: 500px)',
+    bp7: '(min-width: 500px)',
+    bp8: '(min-width: 500px)',
+  },
+
+  theme: {
+    colors: {
+      ...purple,
+    },
+    space: {
+      1: '5px',
+      2: '10px',
+      3: '15px',
+    },
+    fonts: {},
+    fontSizes: {
+      1: '12px',
+      2: '13px',
+      3: '15px',
+    },
+    fontWeights: {},
+  },
+});
