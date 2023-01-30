ddd2aee15cbc0dfd17ba7bcfa385ea5346a673a7
Renato Adorno <renatowillian.2000@gmail.com>
2023-01-30 08:01:57 -0300

    ✨ Feat(next/ts_sass): add new template nexjs

---
+++ next/ts_sass/src/pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+
+// SSG - STATIC SITE GENERATION - use "getStaticProps: GetStaticProps"
+// SSR - SERVER SIDE RENDERING - use "getServerSideProps: GetServerSideProps"
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | New App</title>
+      </Head>
+
+      <main>
+          <h1>👏 New App</h1>
+      </main>
+    </>
+  );
+}
